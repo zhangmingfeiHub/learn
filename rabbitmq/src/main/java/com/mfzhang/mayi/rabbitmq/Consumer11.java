deb1a23c89988d28ea0fd8b09bb9384341ed6056
mingfei.zhang <409244379@qq.com>
2017-12-23 20:23:06 +0800

    A

---
+++ rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Consumer11.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author mingfei.z 2017年12月18日 下午9:33:40
+ */
+package com.mfzhang.mayi.rabbitmq;
+
+/**
+ * 
+ * @author mingfei.z
+ */
+public class Consumer11 {
+
+}
---
+++ rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Producer1.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author mingfei.z 2017年12月18日 下午9:33:14
+ */
+package com.mfzhang.mayi.rabbitmq;
+
+/**
+ * 
+ * @author mingfei.z
+ */
+public class Producer1 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
