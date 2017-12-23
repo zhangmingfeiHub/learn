142e5c65151fcc0b0edf6b7cfef3991ee8e5e7a8
mingfei.zhang <409244379@qq.com>
2017-12-23 23:56:25 +0800

    B

--- rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Consumer12.java
+++ rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Consumer12.java
@@ -4,10 +4,58 @@
  */
 package com.mfzhang.mayi.rabbitmq;
 
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
 /**
  * 
  * @author mingfei.z
  */
 public class Consumer12 {
 
+	public static void main(String[] args) {
+		try {
+			String queue_name = "hello_world";
+			
+			ConnectionFactory connectionFactory = new ConnectionFactory();
+			
+			Connection connection = connectionFactory.newConnection();
+			
+			Channel channel = connection.createChannel();
+			
+			channel.queueDeclare(queue_name, false, false, false, null);
+			
+			Consumer consumer = new DefaultConsumer(channel) {
+				/**
+				 * 
+				 * @author mingfei.z
+				 * @param consumerTag
+				 * @param envelope
+				 * @param properties
+				 * @param body
+				 * @throws IOException
+				 */
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties,
+						byte[] body) throws IOException {
+					String message = new String(body, "UTF-8");
+					System.err.println("msg: " + message);
+				}
+			};
+			
+			channel.basicConsume(queue_name, true, consumer);
+			System.err.println("成功消费2");
+			
+		} catch (Exception e) {
+			System.err.println("e: " + e);
+		}
+	}
+	
 }
--- rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Producer1.java
+++ rabbitmq/src/main/java/com/mfzhang/mayi/rabbitmq/Producer1.java
@@ -4,14 +4,38 @@
  */
 package com.mfzhang.mayi.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 /**
- * 
+ * 消息-生产者
  * @author mingfei.z
  */
 public class Producer1 {
 
 	public static void main(String[] args) {
-		
+		try {
+			String queue_name = "hello_world";
+			
+			ConnectionFactory factory = new ConnectionFactory();
+			
+			Connection connection = factory.newConnection();
+			
+			Channel channel = connection.createChannel();
+			
+			channel.queueDeclare(queue_name, false, false, false, null);
+			
+			String message = "中国客户1";
+			
+			channel.basicPublish("", queue_name, null, message.getBytes("UTF-8"));
+			System.err.println("成功发布");
+			
+			channel.close();
+			connection.close();
+		} catch (Exception e) {
+			System.err.println("e: " + e);
+		}
 	}
 	
 }
