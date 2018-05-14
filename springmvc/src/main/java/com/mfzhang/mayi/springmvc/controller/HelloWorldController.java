3c23e2d6a5c3f1e4d24be36ea6a50bd3aa6e207d
mingfei.zhang <409244379@qq.com>
2018-05-14 22:16:14 +0800

    springmvc 创建子项目

---
+++ springmvc/src/main/java/com/mfzhang/mayi/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author mingfei.z 2018年5月14日 下午10:11:48
+ */
+package com.mfzhang.mayi.springmvc.controller;
+
+/**
+ * 
+ * @author mingfei.z
+ */
+public class HelloWorldController {
+
+}
